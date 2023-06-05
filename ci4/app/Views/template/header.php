a7350da78e4f8e0261cfb833041bb05339e021b9
malfazakki <malfazakki@gmail.com>
2023-06-05 17:49:30 +0700

    update css

--- ci4/app/Views/template/header.php
+++ ci4/app/Views/template/header.php
@@ -5,35 +5,11 @@
   <meta charset="UTF-8">
   <title><?= $title; ?></title>
   <link rel="stylesheet" href="<?= base_url('style.css'); ?>">
-  <!-- <style>
-    @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap");
-
-    * {
-      margin: 0;
-      padding: 0;
-      list-style: none;
-      text-decoration: none;
-      color: black;
-    }
-
-    body {
-      font-family: "Poppins", sans-serif;
-      margin-top: 8vh;
-      box-sizing: border-box;
-    }
-
-    #container {
-      width: 90%;
-      /* box-shadow: 0px 0px 28px -6px rgba(0, 0, 0, 0.75); */
-      margin: 0 auto;
-      height: 100vh;
-    }
-  </style> -->
 </head>
 
 <body>
 
-  <div id="container">
+  <div class="container">
     <header>
       <h1 class="judul">Layout Sederhana</h1>
     </header>
